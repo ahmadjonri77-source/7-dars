776bd9e9e2b5faa4ca186a9ef4138d5ebda95427
Ahmadjon <ahmadjonri77@gmail.com>
2026-04-27 23:53:10 +0500

    dellied key.js

--- src/key.js
+++
@@ -1,8 +0,0 @@
-let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e';
-
-let tokenTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY} `;
-let tokenPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY} `;
-let tokenUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`;
-
-
-       
